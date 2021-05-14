7b79c85615c700b1a99e7f27835551dd8660556b
Aaron Whitfield <aawhitfield@hotmail.com>
2021-05-14 10:50:26 -0700

    add content 5-14-2021

--- lib/diabetic_friendly_foods_breakfast.dart
+++ lib/diabetic_friendly_foods_breakfast.dart
@@ -27,7 +27,7 @@ class DiabeticFriendlyFoodsBreakfast extends StatelessWidget {
                   Container(
                       color: Colors.red,
                       child: Text(
-                        'Quinoa salad with chickpeas,roasted eggplant and feta',
+                        'Step 1: Preheat oven to 200°C. Line a large baking tray with baking paper. Place the eggplant, onion and zucchini on the tray. Spray with oil and season with pepper. Roast for 10 minutes. Add the tomatoes to the tray and roast for a further 10 minutes or until the vegetables are golden and tender',
                         style: TextStyle(color: Colors.white),
                       )),
                 ],
